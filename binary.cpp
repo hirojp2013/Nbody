b240b441bba80af2576156ebd5218f38dee94181
hayashi <0915075t@stu.kobe-u.ac.jp>
2013-01-28 19:09:28 +0900

    binaryのテスト

--- binary.cpp
+++ binary.cpp
@@ -1,8 +1,17 @@
-#include"binary.h"
-binary::binary(){
+#include "binary.h"
+double MIN = 0;
+double MAX = 1.;
+binary::binary():MIN(0.0),MAX(1.0),clh(0.0,1.0){
+  printf("%s(%d)\n",__FILE__,__LINE__);
+  printf("%f,%f\n",MIN,MAX) ;
   ang_vel = new angular_vel;
+
 }
 
 void binary::draw(){
   ang_vel->draw_arrow();
+}
+
+void binary::color_set(double kinetic,GLdouble color[3]){
+  clh.color_map(kinetic,color);
 }
--- binary.h
+++ binary.h
@@ -1,13 +1,16 @@
 #ifndef BINARY_H
 #define BINARY_H
 #include "angular_vel.h"
+#include "color_legend_hue.hpp"
 
 class binary{
  public:
   binary();
   void draw();
+  void color_set(double kinetic,GLdouble color[3]);
  private:
   angular_vel *ang_vel; 
- 
+  color_legend_hue clh;
+  double MIN,MAX;
 };
 #endif
