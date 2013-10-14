69286efdf9a1df3668be8847a880407bebf8e607
Hiroshi Hayashi <135x221x@stu.kobe-u.ac.jp>
2013-10-14 20:30:37 +0900

    Add VisualState class
    Manage visuzlise method by using VisualState class.
    Apply State pattern

---
+++ VisualState.cpp
@@ -0,0 +1,19 @@
+/*
+ * VisualState.cpp
+ *
+ *  Created on: 2013/10/14
+ *      Author: hayashi
+ */
+
+#include "VisualState.h"
+
+VisualState::VisualState() {
+    // TODO Auto-generated constructor stub
+
+}
+
+VisualState::~VisualState() {
+    // TODO Auto-generated destructor stub
+}
+
+
---
+++ VisualState.h
@@ -0,0 +1,18 @@
+/*
+ * VisualState.h
+ *
+ *  Created on: 2013/10/14
+ *      Author: hayashi
+ */
+
+#ifndef VISUALSTATE_H_
+#define VISUALSTATE_H_
+
+class VisualState {
+public:
+    VisualState();
+    virtual ~VisualState();
+    virtual void visuzlize() = 0;
+};
+
+#endif /* VISUALSTATE_H_ */
