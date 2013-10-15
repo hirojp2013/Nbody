30336969c29924d4a177d86253d140c9e1b473df
hayashihiroshi <135x221x@stu.kobe-u.ac.jp>
2013-10-15 16:11:02 +0900

    Add setState(unsigned char key) to Visual class.
    This function is public. This function change states accoring to
    argument charcter.

--- Visualize.cpp
+++ Visualize.cpp
@@ -2,118 +2,156 @@
 //#include "angular_vel.h"
 #include "common.h"
 #include "motion.h"
+#include "VisualState.h"
 #include <cmath>
 
 const double MIN = 0;
 const double MAX = 1.;
-Visualize::Visualize(const double length):clh(MIN,MAX),LENGTH(length){
-  ang_vel = new angular_vel;
-  cm = Common::GetInstance();
+
+Visualize::Visualize() {
+}
+Visualize::Visualize(const double length) :
+		clh(MIN, MAX), LENGTH(length) {
+	ang_vel = new angular_vel;
+	cm = Common::GetInstance();
 }
 
-void Visualize::draw_line(){
-  GLdouble omega[3];
-  Motion *mo = Motion::GetInstance();
-  vector< pair<string,BINARY> >binlist(mo->GetBinaryMap().begin(),
-				       mo->GetBinaryMap().end());
-  vector< pair<string,BINARY> >::iterator bp;
-  BINARY bi_buff;
-  if(cm->binary_state == LINE){
-    glPushMatrix();
-    glRotated(cm->theta, 0.0, 0.0, 1.0);
-    glRotated(cm->phi, 1.0, 0.0, 0.0);
-    glColor3d( 1.0, 1.0, 1.0 );
-    
-    glLineWidth(2.0);
-    glBegin(GL_LINES);
-    for(bp = binlist.begin();bp!=binlist.end();bp++){
-      bi_buff = (*bp).second;
-      glVertex3d(bi_buff.pos[0][0],bi_buff.pos[0][1],bi_buff.pos[0][2]);
-      glVertex3d(bi_buff.pos[1][0],bi_buff.pos[1][1],bi_buff.pos[1][2]);
-    }
-    glEnd();
-    glPopMatrix();
-  }
+void Visualize::setState(unsigned char key) {
+	switch (key) {
+	case 'a':
+		vo.setState(AroundState.getInstance());
+		break;
+	case 'b':
+		vo.setState(ArrowState.getInstance());
+		break;
+	case 'c':
+		vo.setState(EnergyState.getInstance());
+		break;
+	case 'd':
+		vo.setState(NearByState.getInstance());
+		break;
+	case 'e':
+		vo.setState(NoVisualizeState.getInstance());
+		break;
+	case 'f':
+		vo.setState(LineState.getInstance());
+		break;
+	default:
+		exit(1);
+
+	}
+}
+void Visualize::setState(const VisualState& state) {
+	this->state = state;
+}
+void Visualize::draw_line() {
+	GLdouble omega[3];
+	Motion *mo = Motion::GetInstance();
+	vector<pair<string, BINARY> > binlist(mo->GetBinaryMap().begin(),
+			mo->GetBinaryMap().end());
+	vector<pair<string, BINARY> >::iterator bp;
+	BINARY bi_buff;
+	if (cm->binary_state == LINE) {
+		glPushMatrix();
+		glRotated(cm->theta, 0.0, 0.0, 1.0);
+		glRotated(cm->phi, 1.0, 0.0, 0.0);
+		glColor3d(1.0, 1.0, 1.0);
+
+		glLineWidth(2.0);
+		glBegin(GL_LINES);
+		for (bp = binlist.begin(); bp != binlist.end(); bp++) {
+			bi_buff = (*bp).second;
+			glVertex3d(bi_buff.pos[0][0], bi_buff.pos[0][1], bi_buff.pos[0][2]);
+			glVertex3d(bi_buff.pos[1][0], bi_buff.pos[1][1], bi_buff.pos[1][2]);
+
+		}
+		glEnd();
+		glPopMatrix();
+	}
 }
 
-void Visualize::draw_arrow(){
-  if(cm->binary_state == ARROW){
-    printf("state ARROW\n");
-    ang_vel->draw_arrow();
-  }
+void Visualize::draw_arrow() {
+	if (cm->binary_state == ARROW) {
+		printf("state ARROW\n");
+		ang_vel->draw_arrow();
+	}
 }
 
-void Visualize::color_set(double color[][4]){
-  vector<PARTICLE_INF> poslistV = cm->data.getCurrentPosInf();
-  int num=0;
-  switch(cm->binary_state){
-  case NEARBY:
-    glEnable(GL_BLEND);
+void Visualize::color_set(double color[][4]) {
+	vector<PARTICLE_INF> poslistV = cm->data.getCurrentPosInf();
+	int num = 0;
+	switch (cm->binary_state) {
+	case NEARBY:
+		glEnable(GL_BLEND);
 
-    for(vector<PARTICLE_INF>::iterator p_inf=poslistV.begin();p_inf!=poslistV.end();p_inf++){
-      clh.color_map(p_inf->kin,nearby_alpha_boundary(*p_inf),color[num]);
-      num++;
-    }
+		for (vector<PARTICLE_INF>::iterator p_inf = poslistV.begin();
+				p_inf != poslistV.end(); p_inf++) {
+			clh.color_map(p_inf->kin, nearby_alpha_boundary(*p_inf),
+					color[num]);
+			num++;
+		}
 
-    break;
-  case AROUND:
-    glEnable(GL_BLEND);
-    for(vector<PARTICLE_INF>::iterator p_inf=poslistV.begin();p_inf!=poslistV.end();p_inf++){
-      clh.color_map(p_inf->kin,around_alpha_boundary(*p_inf),color[num]);
-      num++;
-    }
-    break;
-  case ENG_SUM:
-    glEnable(GL_BLEND);
-    for(vector<PARTICLE_INF>::iterator p_inf=poslistV.begin();p_inf!=poslistV.end();p_inf++){
-      clh.color_map(eng_sum_color_boundary(*p_inf),
-		    eng_sum_alpha_boundary(*p_inf),
-		    color[num]);
-      num++;
-    }
-    break;
-  default:
-    //    for(vector<PARTICLE_INF>::iterator p_inf=poslistV.begin();p_inf!=poslistV.end();p_inf++){
+		break;
+	case AROUND:
+		glEnable(GL_BLEND);
+		for (vector<PARTICLE_INF>::iterator p_inf = poslistV.begin();
+				p_inf != poslistV.end(); p_inf++) {
+			clh.color_map(p_inf->kin, around_alpha_boundary(*p_inf),
+					color[num]);
+			num++;
+		}
+		break;
+	case ENG_SUM:
+		glEnable(GL_BLEND);
+		for (vector<PARTICLE_INF>::iterator p_inf = poslistV.begin();
+				p_inf != poslistV.end(); p_inf++) {
+			clh.color_map(eng_sum_color_boundary(*p_inf),
+					eng_sum_alpha_boundary(*p_inf), color[num]);
+			num++;
+		}
+		break;
+	default:
+		//    for(vector<PARTICLE_INF>::iterator p_inf=poslistV.begin();p_inf!=poslistV.end();p_inf++){
 
-    for(int i=0;i<PARTICLE_NUMBER_MAX;i++){
-      color[i][0] = 0.0;
-      color[i][1] = 0.0;
-      color[i][2] = 1.0;
-      color[i][3] = 1.0;
-    }
-    
-    break;
-  }
+		for (int i = 0; i < PARTICLE_NUMBER_MAX; i++) {
+			color[i][0] = 0.0;
+			color[i][1] = 0.0;
+			color[i][2] = 1.0;
+			color[i][3] = 1.0;
+		}
+
+		break;
+	}
 }
 
-double Visualize::around_alpha_boundary(PARTICLE_INF par_inf){
-  if(par_inf.l == NOT_MAKE_BINARY){
-    return 0.0;
-  }
-  par_inf.pot = par_inf.pot*LENGTH*2.;
-  printf("par_inf.pot %f\n",par_inf.pot);
-  return ( isinf(par_inf.pot) ? 1.0: par_inf.pot); 
+double Visualize::around_alpha_boundary(PARTICLE_INF par_inf) {
+	if (par_inf.l == NOT_MAKE_BINARY) {
+		return 0.0;
+	}
+	par_inf.pot = par_inf.pot * LENGTH * 2.;
+	printf("par_inf.pot %f\n", par_inf.pot);
+	return (isinf(par_inf.pot) ? 1.0 : par_inf.pot);
 }
 
-double Visualize::eng_sum_alpha_boundary(PARTICLE_INF par_inf){
-  if(par_inf.l == NOT_MAKE_BINARY){
-    return 0.0;
-  }
-  return 1.0;
+double Visualize::eng_sum_alpha_boundary(PARTICLE_INF par_inf) {
+	if (par_inf.l == NOT_MAKE_BINARY) {
+		return 0.0;
+	}
+	return 1.0;
 }
 
-double Visualize::eng_sum_color_boundary(PARTICLE_INF par_inf){
-  if(par_inf.eng_sum > 0){
-    return 0.0;
-  }
-  return fabs(par_inf.eng_sum*LENGTH);
+double Visualize::eng_sum_color_boundary(PARTICLE_INF par_inf) {
+	if (par_inf.eng_sum > 0) {
+		return 0.0;
+	}
+	return fabs(par_inf.eng_sum * LENGTH);
 }
 
-double Visualize::nearby_alpha_boundary(PARTICLE_INF par_inf){
-  if(par_inf.l == NOT_MAKE_BINARY){
-    return 0.0;
-  }
-  double pot = (1.0/par_inf.l)*LENGTH;
+double Visualize::nearby_alpha_boundary(PARTICLE_INF par_inf) {
+	if (par_inf.l == NOT_MAKE_BINARY) {
+		return 0.0;
+	}
+	double pot = (1.0 / par_inf.l) * LENGTH;
 
-  return ( isinf(pot) ? 1.0: pot*1.5); 
+	return (isinf(pot) ? 1.0 : pot * 1.5);
 }
--- Visualize.h
+++ Visualize.h
@@ -7,26 +7,28 @@ class Common;
 class angular_vel;
 struct inf_t;
 
-class Visualize{
- public:
-  Visualize(const double length);
-  void draw_arrow();
-  void draw_line();
-  //  void color_set(struct inf_t p_inf,GLdouble color[4]);
-  void color_set(GLdouble color[][4]);
- private:
-  Common *cm;
-  angular_vel *ang_vel; 
-  color_legend_hue clh;
-  VisualState& state;
-  const static double MIN =0.,MAX = 1.;
-  const double LENGTH; 
-  double around_alpha_boundary(struct inf_t par_inf);
-  double eng_sum_alpha_boundary(struct inf_t par_inf);
-  double eng_sum_color_boundary(struct inf_t par_inf);
-  double nearby_alpha_boundary(struct inf_t par_inf);
+class Visualize {
+public:
+	Visualize();
+	Visualize(const double length);
+	void draw_arrow();
+	void draw_line();
+	void setState(unsigned char key);
+	//  void color_set(struct inf_t p_inf,GLdouble color[4]);
+	void color_set(GLdouble color[][4]);
+private:
+	Common *cm;
+	angular_vel *ang_vel;
+	color_legend_hue clh;
+	VisualState& state;
+	const static double MIN = 0., MAX = 1.;
+	const double LENGTH;
+	double around_alpha_boundary(struct inf_t par_inf);
+	double eng_sum_alpha_boundary(struct inf_t par_inf);
+	double eng_sum_color_boundary(struct inf_t par_inf);
+	double nearby_alpha_boundary(struct inf_t par_inf);
 
-  void setState(const VisualState& state);
-  void requestVisualize();
+	void setState(const VisualState& state);
+	void requestVisualize();
 };
 #endif
