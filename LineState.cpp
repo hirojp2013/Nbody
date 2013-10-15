04deec0d73d5142cb24384493026f5422f8c317f
hayashihiroshi <135x221x@stu.kobe-u.ac.jp>
2013-10-15 15:16:59 +0900

    Work on LineState

--- LineState.cpp
+++ LineState.cpp
@@ -6,36 +6,37 @@
  */
 
 #include "LineState.h"
+using namespace std;
 
 LineState::LineState() {
-    // TODO Auto-generated constructor stub
+	// TODO Auto-generated constructor stub
 
 }
 
 LineState::~LineState() {
-    // TODO Auto-generated destructor stub
+	// TODO Auto-generated destructor stub
 }
 
 void LineState::visualize() {
-    GLdouble omega[3];
-      Motion *mo = Motion::GetInstance();
-      vector< pair<string,BINARY> >binlist(mo->GetBinaryMap().begin(),
-                           mo->GetBinaryMap().end());
-      vector< pair<string,BINARY> >::iterator bp;
-      BINARY bi_buff;
-      if(cm->binary_state == LINE){
-        glPushMatrix();
-        glRotated(cm->theta, 0.0, 0.0, 1.0);
-        glRotated(cm->phi, 1.0, 0.0, 0.0);
-        glColor3d( 1.0, 1.0, 1.0 );
+	GLdouble omega[3];
+	Motion *mo = Motion::GetInstance();
+	std::vector<std::pair<std::string, BINARY> > binlist(
+			mo->GetBinaryMap().begin(), mo->GetBinaryMap().end());
+	vector<pair<string, BINARY> >::iterator bp;
+	BINARY bi_buff;
+	//if(cm->binary_state == LINE){
+	glPushMatrix();
+	glRotated(cm.theta, 0.0, 0.0, 1.0);
+	glRotated(cm.phi, 1.0, 0.0, 0.0);
+	glColor3d(1.0, 1.0, 1.0);
 
-        glLineWidth(2.0);
-        glBegin(GL_LINES);
-        for(bp = binlist.begin();bp!=binlist.end();bp++){
-          bi_buff = (*bp).second;
-          glVertex3d(bi_buff.pos[0][0],bi_buff.pos[0][1],bi_buff.pos[0][2]);
-          glVertex3d(bi_buff.pos[1][0],bi_buff.pos[1][1],bi_buff.pos[1][2]);
-        }
-        glEnd();
-        glPopMatrix();
+	glLineWidth(2.0);
+	glBegin (GL_LINES);
+	for (bp = binlist.begin(); bp != binlist.end(); bp++) {
+		bi_buff = (*bp).second;
+		glVertex3d(bi_buff.pos[0][0], bi_buff.pos[0][1], bi_buff.pos[0][2]);
+		glVertex3d(bi_buff.pos[1][0], bi_buff.pos[1][1], bi_buff.pos[1][2]);
+	}
+	glEnd();
+	glPopMatrix();
 }
--- LineState.h
+++ LineState.h
@@ -8,13 +8,18 @@
 #ifndef LINESTATE_H_
 #define LINESTATE_H_
 
+#include "common.h"
 #include "VisualState.h"
-
+#include <vector>
+#include <string>
+#include <utility>
 class LineState: public VisualState {
 public:
     LineState();
     virtual ~LineState();
     void visualize();
+private:
+    Common& cm;
 };
 
 #endif /* LINESTATE_H_ */
